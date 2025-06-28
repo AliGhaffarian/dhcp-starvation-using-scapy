47f15975b2944b160926a24cdc7b79edc56bde04
user <alighaffarian9@gmail.com>
2025-06-28 09:57:16 +0330

    colorized logger

--- dhcp.py
+++ dhcp.py
@@ -5,15 +5,44 @@ import threading
 import inspect
 import random
 import mac
-import logging
 
 import keepalive
-
-logger = logging.getLogger()
 
 IP_GLOBAL_BROADCAST='255.255.255.255'
 
 PKT_TTL = 5
+
+import logging
+import logging.config
+import colorlog
+import sys
+
+# Define the format and log colors
+log_format = '%(asctime)s [%(levelname)s] %(name)s [%(funcName)s]: %(message)s'
+log_colors = {
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'bold_red',
+        }
+
+# Create the ColoredFormatter object
+console_formatter = colorlog.ColoredFormatter(
+        '%(log_color)s' + log_format,
+        log_colors = log_colors
+        )
+
+FILENAME = os.path.basename(__file__).split('.')[0]
+
+logger = logging.getLogger(FILENAME)
+logger.setLevel(logging.DEBUG)
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setFormatter(console_formatter)
+stdout_handler.setLevel(logging.INFO)
+
+logger.addHandler(stdout_handler)
 
 
 def get_dhcp_type_value(dhcp_type : str):
@@ -40,10 +69,10 @@ def random_transaction_id():
     transaction IDs are random numberes declared by the client
     for the server to be able to differentiate clients 
     """
-    function_name = inspect.currentframe().f_code.co_name
+    
 
     transaction_id = random.randint(0, 0xFFFFFFFF)
-    logger.debug(f"{function_name} : random_transaction_id = {transaction_id}")
+    logger.debug(f"random_transaction_id = {transaction_id}")
     return transaction_id
 
 
@@ -52,14 +81,14 @@ def mac_to_binary(regular_mac : str):
     gets a string, removes collons and turns it to binary representation
     you need to give BOOTP the binary representation of mac in scapy cuz who knows
     """
-    function_name = inspect.currentframe().f_code.co_name
+    
     regular_mac_copy = regular_mac
     
     regular_mac = regular_mac.replace(':', '')
     #BOOTP accepts raw mac for some reason
 
     result = binascii.unhexlify(regular_mac)
-    logger.debug(f"{function_name} : {regular_mac_copy} -> {result}")
+    logger.debug(f"{regular_mac_copy} -> {result}")
 
     return result
 
@@ -67,13 +96,13 @@ def dhcp_request( ip : str, device_mac : str, transaction_id, server_ip : str):
     """
     returns a DHCP request with the source being the args
     """
-    function_name = inspect.currentframe().f_code.co_name
+    
     dhcp_options= [('message-type','request'), ('client_id', device_mac), ('requested_addr', ip),('server_id', server_ip),('end')]
     
 
     request_packet = BOOTP(htype = 'Ethernet (10Mb)', op = 'BOOTREQUEST', chaddr=mac_to_binary(device_mac), xid = transaction_id)\
                     / DHCP(options=dhcp_options)
-    logger.debug(f"{function_name} :  {request_packet.summary()}")
+    logger.debug(f" {request_packet.summary()}")
     return request_packet
 
 def dhcp_discover(src_mac : str):
@@ -92,10 +121,10 @@ def is_bootp(packet)->bool:
     checks if theres bootp
     no other checking
     """
-    function_name = inspect.currentframe().f_code.co_name
+    
 
     if(BOOTP not in packet):
-        logger.warning(f"{function_name} : packet not BOOTP [{packet}]")
+        logger.warning(f"packet not BOOTP [{packet}]")
         return False
 
     return True
@@ -104,20 +133,20 @@ def is_dhcp(packet)->bool:
     """
     checks if theres bootp and dhcp
     """
-    function_name = inspect.currentframe().f_code.co_name
+    
 
     if(is_bootp(packet) == False):
         return False
 
     if(DHCP not in packet):
-        logger.warning(f"{function_name} : packet not DHCP [{packet}]")
+        logger.warning(f"packet not DHCP [{packet}]")
         return False
 
     return True
 
 def is_my_dhcp_offer(packet, my_mac : str)->bool:
     
-    function_name = inspect.currentframe().f_code.co_name
+    
     
     # to binary
     my_mac = mac_to_binary(my_mac)
@@ -135,9 +164,9 @@ def capture_my_dhcp_offer(dest_mac : str, interface : str = conf.iface , result_
     tries to Capture DHCP responses for the provided MAC
     return value of this function doesn't matter as it is intented to provide the result via result_list
     """
-    function_name = inspect.currentframe().f_code.co_name
+    
 
-    logger.info(f"{function_name} : sniffing on interface {interface} for dhcp offers for {dest_mac}")
+    logger.info(f"sniffing on interface {interface} for dhcp offers for {dest_mac}")
     res = sniff(count=1, filter = f"udp and src port {DHCP_ATTS.SERVER_PORT} and dst port {DHCP_ATTS.CLIENT_PORT}", timeout=4, iface=interface)
 
     if len(res) != 0:
@@ -145,8 +174,8 @@ def capture_my_dhcp_offer(dest_mac : str, interface : str = conf.iface , result_
         result_list.append(res[0][Ether].src)
         logger.debug(f"function_name : captured offer for {dest_mac} from {result_list[1]}")
     
-    if len(res) != 0 : logger.info(f"{function_name} : captured {res}")
-    else : logger.warning(f"{function_name} : returning None")
+    if len(res) != 0 : logger.info(f"captured {res}")
+    else : logger.warning(f"returning None")
     
 
 
@@ -176,24 +205,24 @@ def is_dhcp_msg_type_eq(packet, value : int )->bool:
     Won't check if packet is dhcp the caller must do it 
     Searches for dhcp option field and then returns true if message type field of dhcp pdu is equal to value
     """  
-    function_name = inspect.currentframe().f_code.co_name
+    
 
     message_type_index = find_dhcp_option('message-type', packet[DHCP])
 
     #there is no message-type option in dhcp packet
     if message_type_index == -1 : 
         #we assumed the packet is dhcp so there must be a message type
-        logger.warning(f"{function_name} : no message-type in [{packet}]")
+        logger.warning(f"no message-type in [{packet}]")
         return False
 
-    logger.debug(f"{function_name} : message-type in {message_type_index}th index of {packet}")
+    logger.debug(f"message-type in {message_type_index}th index of {packet}")
 
     return packet[DHCP].options[message_type_index][1] == value 
 
 def is_for_my_mac(mac : str, packet):
-    function_name = inspect.currentframe().f_code.co_name
     
-    logger.debug(f"{function_name} : {packet} is destined for {packet[Ether].dst} and provided mac is {mac}")
+    
+    logger.debug(f"{packet} is destined for {packet[Ether].dst} and provided mac is {mac}")
     return packet[Ether].dst == mac
 
 def find_dhcp_option(option : str, dhcp_pdu : DHCP)->int:
@@ -202,24 +231,24 @@ def find_dhcp_option(option : str, dhcp_pdu : DHCP)->int:
     Gets a DHCP PDU and a MAC
     Returns -1 if the DHCP PDU doesn't include the option
     """
-    function_name = inspect.currentframe().f_code.co_name
+    
 
     for i in range (0,len(dhcp_pdu.options)): 
         if dhcp_pdu.options[i][0] == option : return i
     
-    logger.debug(f"{function_name} : {dhcp_pdu} doen't contain {option}")
+    logger.debug(f"{dhcp_pdu} doen't contain {option}")
     return -1
 
 def is_bootp_reply(packet)->bool:
     """
     False if is_bootp() is False or is not a BOOTPREPLY
     """
-    function_name = inspect.currentframe().f_code.co_name
+    
 
     if(is_bootp(packet) == False):
         return False
 
-    logger.debug(f"{function_name} : {packet} packet[BOOTP].op == BOOTREPLY is {packet[BOOTP].op == BOOTP.op.s2i['BOOTREPLY']}")
+    logger.debug(f"{packet} packet[BOOTP].op == BOOTREPLY is {packet[BOOTP].op == BOOTP.op.s2i['BOOTREPLY']}")
     #is a BOOTPREPLY
     return packet[BOOTP].op == BOOTP.op.s2i['BOOTREPLY']
 
@@ -245,12 +274,12 @@ def starve_ips( server_ip : str,server_mac : str ,interface : str = conf.iface ,
 
         template_udp_packet = pkt_to_use
 
-    function_name = inspect.currentframe().f_code.co_name
+    
     
     occupied_ips: list[tuple[str, str]] = []
     is_acked : bool = True
 
-    logger.info(f"{function_name} : starving {ips_to_starve} IPs from {server_ip} , {server_mac}")
+    logger.info(f"starving {ips_to_starve} IPs from {server_ip} , {server_mac}")
 
     time_to_wait = 1
     attempt_n = 0
@@ -260,10 +289,10 @@ def starve_ips( server_ip : str,server_mac : str ,interface : str = conf.iface ,
         
         if(keep_alive and (len(occupied_ips) != 0)):
             keep_alive_thread = threading.Thread(target=keepalive.keep_ips_alive_icmp, args=[occupied_ips, server_ip, server_mac])
-            logger.info(f"{function_name} : keeping alive while starving")
+            logger.info(f"keeping alive while starving")
             keep_alive_thread.start()
 
-        logger.info(f"{function_name} : attempt {attempt_n} captured {len(occupied_ips)} IP's time_to_wait {time_to_wait}")
+        logger.info(f"attempt {attempt_n} captured {len(occupied_ips)} IP's time_to_wait {time_to_wait}")
 
         mac_template = mac.macs[random.randint(0,len(mac.macs) - 1)][0]
 
